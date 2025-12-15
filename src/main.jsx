ad85e20428ca27ed3d86b485d2eefd9a5bcfc22f
Mohamed Rifayath <141167029+mohamedrifa@users.noreply.github.com>
2025-12-15 19:28:59 +0530

    router change

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import App from "./App";
 import { AuthProvider } from "./services/auth";
 import "./index.css"; // <- IMPORTANT: loads the global reset (body { margin: 0; })
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <AuthProvider>
         <App />
       </AuthProvider>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
